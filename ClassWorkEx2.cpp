f355b729e9ddaac338ef1562adb66553397ddaea
NikitaVolia <volanskijnikita2@gmail.com>
2024-03-24 10:04:34 +0100

    fix bug with functions math +, -, /, *

--- ClassWorkEx2.cpp
+++ ClassWorkEx2.cpp
@@ -2,7 +2,13 @@
 #include <iostream>
 #include "Point.h"
 
+using namespace std;
+
 int main()
 {
 	Point a = Point(5, 2, 3);
+	Point b = Point(1, 0, 5);
+	Point c = a + b;
+	
+	cout << c.getX() << " | " << c.getY() << " | " << c.getZ() << std::endl;
 }
--- Point.cpp
+++ Point.cpp
@@ -2,20 +2,20 @@
 
 Point Point::operator+(Point other)
 {
-    return Point(this->x + other.getX(), this->x + other.getY(), this->z + other.getZ());
+    return Point(this->x + other.getX(), this->y + other.getY(), this->z + other.getZ());
 }
 
 Point Point::operator-(Point other)
 {
-    return Point(this->x - other.getX(), this->x - other.getY(), this->z - other.getZ());
+    return Point(this->x - other.getX(), this->y - other.getY(), this->z - other.getZ());
 }
 
 Point Point::operator*(Point other)
 {
-    return Point(this->x * other.getX(), this->x * other.getY(), this->z * other.getZ());
+    return Point(this->x * other.getX(), this->y * other.getY(), this->z * other.getZ());
 }
 
 Point Point::operator/(Point other)
 {
-    return Point(this->x / other.getX(), this->x / other.getY(), this->z / other.getZ());
+    return Point(this->x / other.getX(), this->y / other.getY(), this->z / other.getZ());
 }
